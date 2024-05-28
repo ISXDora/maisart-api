84337b16ed9b83ccd56be0ff3881bb76efaf8609
Isadora Xavier <isinhasena@gmail.com>
2024-05-27 21:06:44 -0300

    tentando conectar com dbpostgres gerenciado

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,14 +7,14 @@ sleep 5
 if ! psql -h $HOST_DB -U $USERNAME_DB -lqt | cut -d \| -f 1 | grep -qw $database; then
   echo "Creating database $database"
   # Adicionar a senha ao comando de criação do banco de dados
-  PGPASSWORD=$PASSWORD_DB rails db:create
+ rails db:create
 else
   echo "Database $database already exists"
 fi
 
 # Executar as migrações
 # Adicionar as credenciais ao comando de migração
-PGPASSWORD=$PASSWORD_DB rails db:migrate
+rails db:migrate
 
 # Executar o servidor Rails (ou outro comando necessário)
 rails server -b 0.0.0.0
