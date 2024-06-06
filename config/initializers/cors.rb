104bd6718cb6c2274d4771659a1f2a7afd0ba57d
Isadora Xavier <isinhasena@gmail.com>
2024-06-06 07:22:56 -0300

    eita Deus apanhando do cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,12 +4,29 @@
 # Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
 
 # Read more: https://github.com/cyu/rack-cors
+# config/initializers/cors.rb
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'https://maisart-web.azurewebsites.net'
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+      max_age: 600
+  end
+end
+
+# Additional logging for debugging
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :options, :head],
+      max_age: 0,
+      expose: ['Access-Control-Allow-Origin', 'Access-Control-Allow-Methods']
   end
 end
+
+Rails.logger.info "CORS configuration: #{Rails.application.config.middleware.inspect}"
